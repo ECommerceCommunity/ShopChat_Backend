4965558b186f5164b749b96e5fc0b785274ab768
dbp-jack <dbp100402@gmail.com>
2025-07-14 16:10:01 +0900

    [MYCE-86] feat/event - EventControllerIntegrationTest @WebMvcTest -> @SpringBootTest + @AutoConfigureMockMvc 사용

--- src/test/java/com/cMall/feedShop/event/presentation/EventControllerIntegrationTest.java
+++ src/test/java/com/cMall/feedShop/event/presentation/EventControllerIntegrationTest.java
@@ -1,38 +1,26 @@
 package com.cMall.feedShop.event.presentation;
 
-import com.cMall.feedShop.event.application.service.EventService;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@WebMvcTest(EventController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class EventControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private EventService eventService;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
+    @DisplayName("이벤트 목록 조회시 200 OK 반환")
     @Test
     void getEventList_ShouldReturnOk() throws Exception {
-        // Given
-        String url = "/api/events";
-
-        // When & Then
-        mockMvc.perform(get(url)
-                .contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/api/events"))
                 .andExpect(status().isOk());
     }
-} 
+}
